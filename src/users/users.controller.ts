f7341627f3dc81f6394934d8a23642d6511ef095
fredydlemus <fredyflemus@gmail.com>
2023-09-12 21:04:05 -0600

    ADD outgoing user response interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,8 +12,11 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -23,7 +26,7 @@ export class UsersController {
   }
 
   @Get('/:id')
-  async indUser(@Param('id') id: string) {
+  async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
 
     if (!user) throw new NotFoundException('user not found');
